907b3d7440208f8d2da7475ceb64deb30b0c6b3f
ahepra@2020spaces.com <Prabhakar.Aher2020spaces.com>
2022-12-08 11:26:37 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Example
+{
+    public static void Main(string[] args)
+    {
+        // Create a list of strings  
+        var countries = new List<string>();
+        countries.Add("India");
+        countries.Add("Australia");
+        countries.Add("Japan");
+        countries.Add("Canada");
+        countries.Add("Mexico");
+
+        // Iterate list element using foreach loop  
+        foreach (var country in countries)
+        {
+            Console.WriteLine(country);
+        }
+    }
+}
